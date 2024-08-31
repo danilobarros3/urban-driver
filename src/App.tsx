22070581378b46b24f8bfc8faa87b5e22ea51af5
danilobarros3 <danilo.lionsoft@gmail.com>
2024-08-31 17:55:28 -0300

    :tada: init: Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Header from "./components/Header/header";
+import "./index.css"
+export default function App() {
+  return (
+    <>
+    <Header/>
+    </>
+  );
+}
---
+++ src/components/Header/header.tsx
@@ -0,0 +1,10 @@
+import { Button } from "../ui/button";
+
+export default function Header(){
+  return(
+    <div>
+      <p className="underline">Header</p>
+      <Button>Teste</Button>
+    </div>
+  )
+}
